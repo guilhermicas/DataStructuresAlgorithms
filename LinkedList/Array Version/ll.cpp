d2248573315bb74c3473d47c40cd8062568a5e09
guilhermicas <sotrabalhosgui@hotmail.com>
2021-02-08 13:59:23 +0000

    Criação da estrutura de ficheiros de linked list

---
+++ LinkedList/Array Version/ll.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class ArrayLinkedList{
+  private:
+    int ARR_SIZE = 16;
+    int LL_array[ARR_SIZE];
+
+  void push(int val){
+
+  }
+
+  void pop(){
+
+  }
+
+  void insertAt(int val, int idx){
+
+  }
+
+  void removeAt(int val, int idx){
+
+  }
+
+  void updateAt(int val, int idx){
+
+  }
+  
+  void peek(){
+
+  }
+
+  void peekAt(int idx){
+
+  }
+}
+
+int main(){
+  /*
+   Ter um array com um tamanho
+   Operações:
+    Inserir(inicio, meio, fim)
+    Remover(inicio, meio, fim)
+    Atualizar dados
+    Visualizar em index
+   */
+  return 1;
+}
---
+++ LinkedList/README.md
@@ -0,0 +1,5 @@
+# Linked List com Array vs Linked List com memória dinâmica
+
+# Complexidade de tempo
+
+# Vantagens e desvantagens entre os dois
