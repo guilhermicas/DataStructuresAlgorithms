0479d8c33974e1cb570b43950183c059939c5e99
guilhermicas <sotrabalhosgui@hotmail.com>
2021-02-08 14:34:35 +0000

    Implementação dos métodos push, pop e showAll
    É notável explicitar que estas implementações são o máximo autónomas possivel, ou seja, sem ou com poucos acessos a recursos exteriores informativos

--- LinkedList/Array Version/ll.cpp
+++ LinkedList/Array Version/ll.cpp
@@ -2,37 +2,52 @@
 
 class ArrayLinkedList{
   private:
-    int ARR_SIZE = 16;
-    int LL_array[ARR_SIZE];
+    static const int ARR_SIZE = 12;
+    int LL_ARRAY[ARR_SIZE];
+    int end = -1;
 
-  void push(int val){
+  public:
+    void push(int val){
+      //Adiciona um valor no fim da lista
+      end += 1;
+      LL_ARRAY[end] = val;
+    }
 
-  }
+    void showAll(){
+      //Mostra todos os valores da lista
+      for(int i = 0; i < ARR_SIZE; i++){
+        std::cout << this->LL_ARRAY[i] << std::endl;
+      }
+    }
 
-  void pop(){
+    int pop(){
+      //Devolve e remove o ultimo elemento da lista
+      int returnValue = LL_ARRAY[end];
+      LL_ARRAY[end] = 0;
+      end -= 1;
+      return returnValue;
+    }
 
-  }
+    void insertAt(int val, int idx){
 
-  void insertAt(int val, int idx){
+    }
 
-  }
+    void removeAt(int val, int idx){
 
-  void removeAt(int val, int idx){
+    }
 
-  }
+    void updateAt(int val, int idx){
 
-  void updateAt(int val, int idx){
+    }
+    
+    void peek(){
 
-  }
-  
-  void peek(){
+    }
 
-  }
+    void peekAt(int idx){
 
-  void peekAt(int idx){
-
-  }
-}
+    }
+};
 
 int main(){
   /*
@@ -43,5 +58,12 @@ int main(){
     Atualizar dados
     Visualizar em index
    */
+
+  ArrayLinkedList* arr = new ArrayLinkedList();
+  arr->push(10);
+  arr->push(30);
+  int lastVal = arr->pop();
+  arr->showAll();
+  std::cout << "Last Val = " << lastVal << std::endl;
   return 1;
 }
