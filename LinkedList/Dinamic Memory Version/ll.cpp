eae14901d53f7d3d10a89833f58fdc707a6a93ee
guilhermicas <sotrabalhosgui@hotmail.com>
2021-03-26 15:42:12 +0000

    funcionalidades ll

--- LinkedList/Dinamic Memory Version/ll.cpp
+++ LinkedList/Dinamic Memory Version/ll.cpp
@@ -13,10 +13,56 @@ class Node{
 class LinkedList{
   private:
     Node *head;
+
+  public:
+    LinkedList();
+    LinkedList(Node* head);
+    bool isEmpty();
+    void printHead();
+    void append(Node* newNode);
 };
+
+LinkedList::LinkedList(){
+  this->head = NULL;
+}
+
+LinkedList::LinkedList(Node* head){
+  this->head = head;
+}
+
+bool LinkedList::isEmpty(){
+  if(this->head == NULL){
+    return true;
+  }
+  return true;
+}
+
+void LinkedList::printHead(){
+  if(this-isEmpty()){
+    std::cout<<"There is no head in this linked list, you should append one first"<<std::endl;
+  }else{
+    std::cout<<this->head<<std::endl;
+  }
+}
+
+void LinkedList::append(Node* newNode){
+  if(this->isEmpty()){
+    this->head = newNode;
+  }else{
+    Node *curr = this->head;
+
+    while(curr->next != NULL){
+      curr = curr->next;
+    }
+
+    curr->next = newNode;
+  }
+}
 
 int main(){
   Node *newNode = new Node(10);
-  LinkedList *ll = new LinkedList(newNode);
+  LinkedList *ll = new LinkedList();
+  ll->printHead();
+  ll->append(newNode);
   ll->printHead();
 }
