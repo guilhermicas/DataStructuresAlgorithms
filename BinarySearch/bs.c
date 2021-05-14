f3e15ee3dd59546874699c40c15664965cec11f4
guilhermicas <sotrabalhosgui@hotmail.com>
2021-05-14 17:15:51 +0100

    Optimization and fixes

--- BinarySearch/bs.c
+++ BinarySearch/bs.c
@@ -1,66 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int array[100];
-int arrLen = sizeof(array) / sizeof(array[0]);
-
 // Outputs array
-void printArray(int arr[]){
-  for(int i = 0; i < arrLen; i++){
+void printArray(int arr[], int arrSize){
+  for(int i = 0; i < arrSize; i++){
     //printf("nr%d: %d\n", i, arr[i]);
     printf("%d ", arr[i]);
   }
   printf("\n");
 }
 
-void arrayInit(int arr[]){
-  for(int i = 0; i < arrLen; i++){
-    array[i] = i + 10;
-  }
-}
-
-int binarySearch(int arr[], int num){
+int binarySearch(int searchValue, int arr[], int arrSize){
   /*
    @brief - Binary Search
+   @param searchValue - The value that will be searched
    @param arr[] - Array in which the search will be made
-   @param num - The value that will be searched
+   @param arrSize - Size of the array
    @return - Returns the index of the number if it found it, else -1
   */
-  int bot = 0;
-  int top = arrLen - 1;
-  int mid = top / 2;
-  int midVal;
+  int low = 0;
+  int high = arrSize - 1;
 
   //if top < bot then it didnt find number in array
-  while(top > bot){
-    //Helper variables
-    int tmp = 0;
-    midVal = arr[mid];
+  while(low <= high){
+    int mid = low + (high - low) / 2;
+    printf("low = %d\nhigh = %d\nmid = %d\n", low, high, mid);
 
     //The search
-    if(num > midVal){
-      tmp = bot;
-      bot = mid;
-      mid = top - ((top - tmp) / 2);
-      continue;
-    }else if(num < midVal){
-      tmp = top;
-      top = mid;
-      mid = top + ((bot - tmp) / 2);
-      continue;
-    }else if(num == midVal){
-      printf("[Value %d found at index %d]\n", midVal, mid);
+    if(searchValue == arr[mid]){
+      printf("[Value %d found at index %d]\n", arr[mid], mid);
       return mid;
+    }else if(searchValue > arr[mid]){
+      low = mid + 1;
+    }else if(searchValue < arr[mid]){
+      high = mid - 1;
     }
-
-    printf("[Value not found]\n");
-    break;
   }
 
   //If it exits out of loop it means it didnt find in array
+  printf("[Value not found]\n");
   return -1;
 
-  //printf("bot = %d\ntop = %d\nmid = %d\n", bot, top, mid);
 }
 
 int main(int argc, char **argv[]){
@@ -68,22 +48,21 @@ int main(int argc, char **argv[]){
     Binary search in a sorted int array
   */
 
-  //Initializes array values
-  arrayInit(array);
+  int array[] = {1, 3, 7, 12, 15, 22, 23};
+  int arrayLen = sizeof(array) / sizeof(array[0]);
 
-  printArray(array);
-  
+  printArray(array, arrayLen);
+
   //Search interaction
   printf("Value to search = ");
 
   int n;
   scanf("%d", &n);
 
-  int res = binarySearch(array, n);
+  int res = binarySearch(n, array, arrayLen);
 
   //Objective find index of n
   printf("The function returns:\n%d\n", res);
 
   return 1;
 }
-
